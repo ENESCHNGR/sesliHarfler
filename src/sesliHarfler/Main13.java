763803fb4be5cd6bd8bf40ce1d4ef59b4b9c1c17
ENESCHNGR <113687747+ENESCHNGR@users.noreply.github.com>
2022-09-28 19:15:33 +0300

    sesliHarfler

---
+++ src/sesliHarfler/Main13.java
@@ -0,0 +1,22 @@
+package sesliHarfler;
+
+public class Main13 {
+
+	public static void main(String[] args) {
+      char harf = 'E';
+      
+      switch(harf) {
+      case 'A':
+      case 'I':
+      case 'O':
+      case 'U':
+    	  System.out.println("Kalın sesli harfler");
+    	  break;
+    	  default:
+    	  System.out.println("İnce sesli harfler");
+
+      }
+      
+	}
+
+}
